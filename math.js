66cbc19433b42f9cd1e9c9c082768b105034e64b
Dancan Wambia Njuguna <dante@Dancans-MacBook-Pro.local>
2025-12-02 10:24:29 +0300

    update

---
+++ math.js
@@ -0,0 +1,8 @@
+const utils = require('./utils');
+
+const num1 = 10;
+const num2 = 15;
+
+console.log(`Sum of ${num1} and ${num2} is ${utils.calculateSumOfTwoNumbers(num1,num2)}`);
+console.log(`Multiplication of ${num1} and ${num2} is ${utils.multiplyTwoNumbers(num1,num2)}`);
+console.log(`Division of ${num1} and ${num2} is ${utils.divideTwoNumbers(num1,num2)}`);
---
+++ utils.js
@@ -0,0 +1,17 @@
+function calculateSumOfTwoNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+function multiplyTwoNumbers(num1, num2) {
+    return num1 * num2;
+}
+
+function divideTwoNumbers(num1, num2) {
+    return num1 / num2;
+}
+
+module.exports = {
+    calculateSumOfTwoNumbers,
+    multiplyTwoNumbers,
+    divideTwoNumbers
+}
